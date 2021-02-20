55a703ba57130e387071eda262cb8f604a5a14f8
Maflo88 <77553105+Maflo88@users.noreply.github.com>
2021-02-20 19:21:29 -0300

    Add files via upload

---
+++ app/js/script.js
@@ -0,0 +1,41 @@
+const toggle = document.getElementById('toggle-input');
+let subtitle = document.querySelector('h2');
+
+/*The function adds the appropriate class to the document 
+root based on the theme argument. 
+If persist is set, it'll store the setting in localStorage.*/
+function setTheme(theme) {
+  const on = theme;
+  const off = theme === 'light' ? 'dark' : 'light'
+
+  const htmlEl = document.documentElement;
+  htmlEl.classList.add(on);
+  htmlEl.classList.remove(off);
+
+}
+
+function updateUI(theme) {
+  toggle.checked = theme === 'dark';
+
+  if (theme === 'light') {
+    subtitle.classList.remove('dark-theme');
+    subtitle.classList.add('light-theme');
+  } else {
+    subtitle.classList.add('dark-theme');
+    subtitle.classList.remove('light-theme');
+  }
+}
+
+toggle.addEventListener('click', () => {
+  const theme = toggle.checked ? 'dark' : 'light';
+  setTheme(theme);
+  updateUI(theme);
+});
+
+/* Set theme on load */
+const osPreference = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+const preferredTheme = localStorage.getItem('preferred-theme') || osPreference;
+setTheme(preferredTheme);
+updateUI(preferredTheme);
+
+/*Source: https://codepen.io/radekpazdera/pen/zYBNLPo by Radek Pazdera*/
